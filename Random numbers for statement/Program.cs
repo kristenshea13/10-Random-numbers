7b5b2a2f640ca5795d02ba5cd0dd899caef4fd24
Kristen Shea <kstar61@live.com>
2018-08-29 23:20:35 -0500

    10 random numbers from a range of integers using a for statement

--- Random numbers for statement/Program.cs
+++ Random numbers for statement/Program.cs
@@ -6,7 +6,43 @@ namespace Random_numbers_for_statement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Ten random numbers between 0 and 100:");
+
+
+
+            Random rnd = new Random();
+           
+            
+            int randomTally = 0;
+            //declare randomTally as int and set to 0 as a baseline
+
+                for (randomTally = 1; randomTally <= 10; randomTally++)
+                {
+                    Console.WriteLine($"Random number {randomTally}: ");
+                    Console.WriteLine(rnd.Next(0, 101));
+                    //max number is not inclusive, if i wanted to choose between 0 and 100, had to set 101 as max
+                }
+                
+              
+             
+              
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
         }
+
+       
+
+
     }
 }

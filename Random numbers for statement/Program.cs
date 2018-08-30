439aaad9a6de2110f7cc4ac597d9f59811bae9b8
Kristen Shea <kstar61@live.com>
2018-08-29 22:19:57 -0500

    Add project files.

---
+++ Random numbers for statement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Random_numbers_for_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
